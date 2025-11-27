df434ae629e838cb65f143ca8e83f9b49503c4f5
Rock Chong <chongcr128@gmail.com>
2025-11-28 02:56:34 +0800

    feat: add investor CRUD routes to auth middleware

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,7 @@ Route::get('/instructions', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get("investors", [InvestorController::class, 'index'])->name('investor.index');
-Route::get("investors/create", [InvestorController::class, 'create'])->name('investor.create');
-Route::post("investors", [InvestorController::class, 'store'])->name('investor.store');
-Route::get("investors/{investor}/edit", [InvestorController::class, 'edit'])->name('investor.edit');
-Route::put("investors/{investor}", [InvestorController::class, 'update'])->name('investor.update');
-
-Route::get("funds", [FundController::class, 'index'])->name('funds.index');;
+Route::get("funds", [FundController::class, 'index'])->name('funds.index');
 Route::get("investments", [InvestmentController::class, 'index'])->name('investments.index');
 Route::get("graph", [GraphController::class, 'index'])->name('graph.index');
 Route::get('/exam', function () {
@@ -32,6 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get("investors/create", [InvestorController::class, 'create'])->name('investor.create');
+    Route::post("investors", [InvestorController::class, 'store'])->name('investor.store');
+    Route::get("investors/{investor}/edit", [InvestorController::class, 'edit'])->name('investor.edit');
+    Route::put("investors/{investor}", [InvestorController::class, 'update'])->name('investor.update');
+
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
