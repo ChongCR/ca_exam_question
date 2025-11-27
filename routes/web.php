f805c5eec622a7df10cabd1536dc3b14246986ab
Rock Chong <chongcr128@gmail.com>
2025-11-28 02:43:48 +0800

    chore: Update routes, seeders, and dependencies

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\FundController;
 use App\Http\Controllers\GraphController;
 use App\Http\Controllers\InvestmentController;
+use App\Http\Controllers\InvestorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,12 @@ Route::get('/instructions', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get("investors", [\App\Http\Controllers\InvestorController::class, 'index'])->name('investors.index');
+Route::get("investors", [InvestorController::class, 'index'])->name('investor.index');
+Route::get("investors/create", [InvestorController::class, 'create'])->name('investor.create');
+Route::post("investors", [InvestorController::class, 'store'])->name('investor.store');
+Route::get("investors/{investor}/edit", [InvestorController::class, 'edit'])->name('investor.edit');
+Route::put("investors/{investor}", [InvestorController::class, 'update'])->name('investor.update');
+
 Route::get("funds", [FundController::class, 'index'])->name('funds.index');;
 Route::get("investments", [InvestmentController::class, 'index'])->name('investments.index');
 Route::get("graph", [GraphController::class, 'index'])->name('graph.index');
